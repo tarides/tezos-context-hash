e88ea3969f64eaca056b6acbbc1a1e303d806238
mattiasdrp <5543639+mattiasdrp@users.noreply.github.com>
2021-02-09 16:17:30 +0100

    Added progress bars and a verbose mode

--- inode_hash/inode_hash.ml
+++ inode_hash/inode_hash.ml
@@ -7,6 +7,14 @@ module H_contents = Irmin.Hash.Typed (Hash) (Contents)
 let contents x = `Contents (x, Metadata.default)
 let node x = `Node x
 
+let progress n =
+  let bar message =
+    Progress_unix.counter ~mode:`UTF8 ~total:(Int64.of_int n) ~message
+      ~pp:Progress.Units.bytes ()
+  in
+  Progress_unix.(with_reporters (bar "Generating     " / bar "Serialising    "))
+  @@ fun (g, s) b () -> match b with `Generating -> g | `Serialising -> s
+
 module Gen = struct
   (* let init = Random.init
    * let full_init = Random.full_init *)
@@ -15,7 +23,14 @@ module Gen = struct
   let string () = fixed_string (Random.int (1 lsl 10)) ()
   let fixed_bytes n () = Bytes.init n (fun _ -> char ())
   let bytes () = fixed_bytes (Random.int (1 lsl 10)) ()
-  let fixed_list n gen () = List.init n (fun _ -> gen ())
+
+  let fixed_list ?(verbose = false) ?prog n gen () =
+    List.init n (fun _ ->
+        (match prog with
+        | None -> ()
+        | Some prog -> if verbose then (prog ()) 1L);
+        gen ())
+
   let list gen () = fixed_list (Random.int (1 lsl 10)) gen ()
   let pair gen1 gen2 () = (gen1 (), gen2 ())
   let content () = bytes ()
@@ -33,18 +48,27 @@ module Gen = struct
     fixed_inode len ()
 end
 
-let to_json inodes : bytes =
-  Bytes.of_string (String.concat "\n" (List.map Inter.Val.Serde.from_t inodes))
+let to_json verbose prog inodes : bytes =
+  Bytes.of_string
+    (String.concat "\n"
+       (List.map
+          (fun t ->
+            if verbose then (prog ()) 1L;
+            Inter.Val.Serde.from_t t)
+          inodes))
 
-let run n short long path =
-  if short && long then invalid_arg "short and long options are incompatible";
+let run n inodes_type path verbose =
   let oc = open_out path in
-  Gen.fixed_list n
-    (if short then Gen.short_inode
-    else if long then Gen.long_inode
-    else Gen.inode)
+  let progress = progress n in
+  Gen.fixed_list ~verbose
+    ~prog:(progress `Generating)
+    n
+    (match inodes_type with
+    | `Short -> Gen.short_inode
+    | `Long -> Gen.long_inode
+    | `Normal -> Gen.inode)
     ()
-  |> to_json
+  |> to_json verbose (progress `Serialising)
   |> output_bytes oc;
   close_out oc
 
@@ -54,22 +78,27 @@ let inodes_number =
   let doc = "Number of inodes to generate." in
   Arg.(value & opt int 1_000 & info [ "n"; "inodes-number" ] ~doc)
 
-let short_inodes =
-  let doc = "Only generate short inodes." in
-  Arg.(value & opt bool false & info [ "short" ] ~doc)
-
-let long_inodes =
-  let doc = "Only generate long inodes." in
-  Arg.(value & opt bool false & info [ "long" ] ~doc)
+let inodes_type =
+  let doc = "Normal inode." in
+  let normal = (`Normal, Arg.info [ "normal" ] ~doc) in
+  let doc = "Short inode." in
+  let short = (`Short, Arg.info [ "short" ] ~doc) in
+  let doc = "Long inode." in
+  let long = (`Long, Arg.info [ "long" ] ~doc) in
+  Arg.(value & vflag `Normal [ normal; short; long ])
 
 let path =
   let doc = "The file where the result will be printed." in
   Arg.(value & opt string "inodes.json" & info [ "o"; "output" ] ~doc)
 
+let verbose =
+  let doc = "Be verbose." in
+  Arg.(value & flag & info [ "v"; "verbose" ] ~doc)
+
 let cmd =
   let doc = "Irmin inodes generation" in
   Term.
-    ( const run $ inodes_number $ short_inodes $ long_inodes $ path,
+    ( const run $ inodes_number $ inodes_type $ path $ verbose,
       info "inodes-gen" ~doc )
 
 let () = Term.(exit @@ eval cmd)
