93cc563b0833fd34c425e9a0725e940ca1ff2a39
Clément Pascutto <clement@pascutto.fr>
2021-03-22 09:07:02 +0100

    Add fuzzing
    Co-authored-by: Nicolas Osborne <nicolas.osborne.etu@univ-lille.fr>

--- test/main.ml
+++ test/main.ml
@@ -17,7 +17,13 @@ end
 
 module C : Testable = Spec
 
-module R : Testable = struct
+module R :
+  Testable
+    with type hash = C.hash
+     and type commit_metadata = C.commit_metadata
+     and type commit = C.commit
+     and type entry_kind = C.entry_kind
+     and type tree_entry = C.tree_entry = struct
   type hash = string
 
   let with_encoder encoder x =
@@ -45,8 +51,17 @@ module R : Testable = struct
     in
     with_encoder encode
 
-  type commit_metadata = { date : int64; author : string; message : string }
-  type commit = { tree : hash; parents : hash list; metadata : commit_metadata }
+  type commit_metadata = C.commit_metadata = {
+    date : int64;
+    author : string;
+    message : string;
+  }
+
+  type commit = C.commit = {
+    tree : hash;
+    parents : hash list;
+    metadata : commit_metadata;
+  }
 
   let commit =
     let encode =
@@ -62,13 +77,24 @@ module R : Testable = struct
     in
     fun c -> with_encoder encode (to_irmin_commit c)
 
-  type entry_kind = Content | Node
-  type tree_entry = { name : string; kind : entry_kind; hash : hash }
+  type entry_kind = C.entry_kind = Content | Node
+
+  type tree_entry = C.tree_entry = {
+    name : string;
+    kind : entry_kind;
+    hash : hash;
+  }
 
   let ocaml_hash = Hashtbl.seeded_hash
 
+  module Inter =
+    Irmin_pack.Private.Inode.Make_intermediate
+      (Irmin_tezos.Conf)
+      (Irmin_tezos.Encoding.Hash)
+      (Irmin_tezos.Encoding.Node)
+
   let tree =
-    let encode = Irmin.Type.(unstage (pre_hash Irmin_tezos.Encoding.Node.t)) in
+    let encode = Irmin.Type.(unstage (pre_hash Inter.Val.t)) in
     let to_irmin_entry { name; kind; hash } =
       let hash = to_irmin_hash hash in
       ( name,
@@ -76,28 +102,103 @@ module R : Testable = struct
         | Content -> `Contents (hash, Irmin_tezos.Encoding.Metadata.default)
         | Node -> `Node hash )
     in
-    let to_irmin_tree l =
-      List.map to_irmin_entry l |> Irmin_tezos.Encoding.Node.v
-    in
+    let to_irmin_tree l = List.map to_irmin_entry l |> Inter.Val.v in
     fun t -> with_encoder encode (to_irmin_tree t)
+end
+
+(** Custom generators absent from Monolith *)
+module G = struct
+  let hash () = Gen.string (fun () -> 32) Gen.char ()
+  let string () = Gen.string (Gen.closed_interval 1 64) Gen.char ()
+  let bytes () = Bytes.of_string (string ())
+  let int64 () = Int64.of_int (Gen.int Int.max_int ())
+
+  let commit_metadata () =
+    C.{ date = int64 (); author = string (); message = string () }
+
+  let commit () =
+    {
+      C.tree = hash ();
+      C.parents = Gen.list (Gen.closed_interval 1 32) hash ();
+      C.metadata = commit_metadata ();
+    }
+
+  let tree_entry () =
+    {
+      C.name = string ();
+      C.kind = Gen.choose [ C.Content; C.Node ] ();
+      C.hash = hash ();
+    }
+end
+
+(** Custom printers absent from PPrint *)
+module P = struct
+  open PPrintOCaml
+
+  let commit_metadata C.{ date; author; message } =
+    record ""
+      [
+        ("date", int64 date);
+        ("author", string author);
+        ("message", string message);
+      ]
+
+  let commit C.{ tree; parents; metadata } =
+    record ""
+      [
+        ("tree", string tree);
+        ("parents", (list string) parents);
+        ("metadata", commit_metadata metadata);
+      ]
+
+  let tree_entry C.{ name; kind; hash } =
+    record ""
+      [
+        ("name", string name);
+        ( "kind",
+          string (match kind with Node -> "Node" | Content -> "Content") );
+        ("hash", string hash);
+      ]
 end
 
 let positive_int = int_within (Gen.int Int.max_int)
 
 let bytes =
-  easily_constructible
-    (fun () ->
-      Bytes.unsafe_of_string (Gen.string (Gen.int (1 lsl 5)) Gen.char ()))
-    (fun b -> PPrintOCaml.string (Bytes.to_string b))
+  easily_constructible G.bytes (fun b ->
+      PPrintOCaml.string (Bytes.unsafe_to_string b))
 
 let string =
-  let neg =
-    easily_constructible
-      (Gen.string (Gen.int (1 lsl 5)) Gen.char)
-      PPrintOCaml.string
-  in
-  let pos = deconstructible PPrint.string in
+  let neg = easily_constructible G.string PPrintOCaml.string in
+  let pos = deconstructible PPrintOCaml.string in
   ifpol neg pos
+
+let int64 =
+  easily_constructible G.int64 (fun i -> PPrint.string (Int64.to_string i))
+
+let commit_metadata = easily_constructible G.commit_metadata P.commit_metadata
+let commit = easily_constructible G.commit P.commit
+let tree_entry = easily_constructible G.tree_entry P.tree_entry
+
+let distinct_names =
+  let tbl = Hashtbl.create 0 in
+  fun l ->
+    Hashtbl.clear tbl;
+    let rec loop = function
+      | [] -> true
+      | h :: t ->
+          let name = h.C.name in
+          if Hashtbl.mem tbl name then false
+          else (
+            Hashtbl.add tbl name ();
+            loop t)
+    in
+    loop l
+
+let short_entry_list =
+  distinct_names % list ~length:(Gen.closed_interval 1 256) tree_entry
+
+let long_entry_list =
+  distinct_names % list ~length:(Gen.closed_interval 257 1024) tree_entry
 
 let () =
   let spec = positive_int ^> string in
@@ -107,10 +208,19 @@ let () =
   declare "LEB128 int" spec R.leb128_int C.leb128_int;
 
   let spec = bytes ^> string in
-  declare "contents" spec R.content C.content;
+  declare "content" spec R.content C.content;
 
-  let spec = string ^> int in
-  declare "ocaml_hash" spec (R.ocaml_hash 0) (C.ocaml_hash 0)
+  let spec = commit ^> string in
+  declare "commit" spec R.commit C.commit;
+
+  let spec = positive_int ^> string ^> int in
+  declare "ocaml_hash" spec R.ocaml_hash C.ocaml_hash;
+
+  let spec = short_entry_list ^> string in
+  declare "node" spec R.tree C.tree;
+
+  let spec = long_entry_list ^> string in
+  declare "inode" spec R.tree C.tree
 
 let () =
   let fuel = 10 in
