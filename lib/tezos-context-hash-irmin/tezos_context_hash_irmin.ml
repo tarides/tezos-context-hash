f68cd4a3de4d8037c98d0023b5b70de2e5f26b67
Thomas Gazagnaire <thomas@gazagnaire.org>
2021-07-15 17:13:06 +0200

    Fix formatting

--- lib/tezos-context-hash-irmin/tezos_context_hash_irmin.ml
+++ lib/tezos-context-hash-irmin/tezos_context_hash_irmin.ml
@@ -11,13 +11,6 @@ module Conf = struct
 end
 
 module Store =
-  Irmin_pack.Make_ext
-    (V1)
-    (Conf)
-    (Node)
-    (Commit)
-    (Metadata)
-    (Contents)
-    (Path)
+  Irmin_pack.Make_ext (V1) (Conf) (Node) (Commit) (Metadata) (Contents) (Path)
     (Branch)
     (Hash)
